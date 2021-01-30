3a747fa2b3009108048a1153bc9146e615fd9f40
Apoorv Taneja <apoorvtaneja@Apoorvs-Air.domain.name>
2021-01-30 18:50:11 +0530

    added production link

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://hangman-multiplayer.herokuapp.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
